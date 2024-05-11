1df87faebdc58415aa0d4be34d31bfc45ff5d1e8
intikhab-h-bhat <intikhabh@gmail.com>
2024-05-11 13:09:02 +0530

    style change

--- src/script.js
+++ src/script.js
@@ -1,28 +1,29 @@
-const count = document.querySelector(".count");
-const buttons = document.querySelector(".buttons");
+const container = document.querySelector(".container");
+const joinBtn = document.querySelector(".navigation a:nth-child(3)");
+const homeBtn = document.querySelector(".navigation a:nth-child(2)");
+const loginBtn = document.querySelector(".contact p a");
+const loginMsg = document.querySelector(".contact p span");
+const loginHeader = document.querySelector(".contact h1");
+const contactBtn = document.querySelector(".contact-btn:nth-child(2)");
+const contactForm = document.querySelector(".contact-form");
 
-buttons.addEventListener("click", (e) => {
-  console.log(e.target.classList);
-
-  if (e.target.classList.contains("add")) {
-    count.innerHTML++;
-  }
-  if (e.target.classList.contains("subtract")) {
-    count.innerHTML--;
-  }
-  if (e.target.classList.contains("reset")) {
-    count.innerHTML = 0;
-  }
-
-  setColor();
+joinBtn.addEventListener("click", () => {
+  container.classList.add("change");
 });
 
-function setColor() {
-  if (count.innerHTML > 0) {
-    count.style.color = "Blue";
-  } else if (count.innerHTML < 0) {
-    count.style.color = "orangered";
-  } else {
-    count.style.color = "#fff";
-  }
-}
+homeBtn.addEventListener("click", () => {
+  container.classList.remove("change");
+});
+
+loginBtn.addEventListener("click", () => {
+  contactForm.reset();
+  container.classList.toggle("hide")
+    ? (loginBtn.textContent = "Sign Up") &&
+      (contactBtn.textContent = "Log In") &&
+      (loginMsg.textContent = "Don't have an account?") &&
+      (loginHeader.textContent = "Welcome Back")
+    : (loginBtn.textContent = "Log In") &&
+      (contactBtn.textContent = "Create Account") &&
+      (loginMsg.textContent = "Already have an account?") &&
+      (loginHeader.textContent = "Create New Account");
+});
